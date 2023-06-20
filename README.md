75656a65ad77e4f1012298b721fcaaceec3af821
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2023-06-21 01:25:05 +0530

    Fixed the delay and changes readme file and css

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Hospital-site
-It is hospital site. In this i used magnific popup and AOS for the first time.
+
+
+# Hospital Website
+
+Welcome to the repository for the Hospital Website project. This repository contains the HTML, CSS, and JavaScript files for the Hospital Website, which is a web application designed to provide information and services to patients, healthcare professionals, and the general public.
+ 
+Here I have used magnific popup and AOS for the first time
+
+## Magnific Popup
+
+The Hospital Website utilizes the Magnific Popup library for displaying image galleries and videos. The necessary JavaScript and CSS files for Magnific Popup are included in the `libs/` directory. To use Magnific Popup, refer to the documentation provided by the library: [Magnific Popup Documentation](https://dimsemenov.com/plugins/magnific-popup/documentation.html). You can customize the gallery and video settings by modifying the appropriate JavaScript code in the `scripts.js` file.
+
+## AOS (Animate On Scroll)
+
+The Hospital Website uses the AOS library to animate elements as they scroll into view. AOS provides various animation effects and configuration options to enhance the user experience. The necessary JavaScript and CSS files for AOS are included in the `libs/` directory. To use AOS, refer to the documentation provided by the library: [AOS Documentation](https://michalsnik.github.io/aos/). You can customize the animation settings by modifying the appropriate JavaScript code in the `scripts.js` file
+
+## Contributing
+
+Contributions to the Hospital Website project are welcome! If you have any improvements, bug fixes, or new features to propose, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch: `git checkout -b feature/your-feature-name`.
+3. Make your changes and commit them: `git commit -m 'Add some feature'`.
+4. Push to the branch: `git push origin feature/your-feature-name`.
+5. Submit a pull request.
+
+i will feel Happy If anyone Likes to imporve the website look..................
+
+
--- main.js
+++ main.js
@@ -1,28 +1,59 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $('.fa-bars').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.nav').toggleClass('nav-toggle');
+//     $('.fa-bars').click(function(){
+//         $(this).toggleClass('fa-times');
+//         $('.nav').toggleClass('nav-toggle');
+//     });
+
+//     $(window).on('load scroll',function(){
+
+//         $('.fa-bars').removeClass('fa-times');
+//         $('.nav').removeClass('nav-toggle');
+
+//         if($(window).scrollTop()>10){
+//             $('header').addClass('header-active');
+//         }
+//         else{
+//             $('header').removeClass('header-active');
+//         }
+//     });
+
+//     $('.facility').magnificPopup({
+//         delegate: 'a',
+//         type: 'image',
+//         gallery:{
+//             enabled: true
+//         }
+//     });
+// });
+
+$(document).ready(function() {
+    $('.fa-bars').click(function() {
+      $(this).toggleClass('fa-times');
+      $('.nav').toggleClass('nav-toggle');
     });
-
-    $(window).on('load scroll',function(){
-
-        $('.fa-bars').removeClass('fa-times');
-        $('.nav').removeClass('nav-toggle');
-
-        if($(window).scrollTop()>10){
-            $('header').addClass('header-active');
-        }
-        else{
-            $('header').removeClass('header-active');
-        }
+  
+    $(window).on('load scroll', function() {
+      $('.fa-bars').removeClass('fa-times');
+      $('.nav').removeClass('nav-toggle');
+  
+      if ($(window).scrollTop() > 10) {
+        $('header').addClass('header-active');
+      } else {
+        $('header').removeClass('header-active');
+      }
     });
-
+  
+    AOS.init({
+      delay: 100, // Set the delay to 100ms (default: 0)
+      duration: 800 // Set the duration to 800ms (default: 1000)
+    });
+  
     $('.facility').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery:{
-            enabled: true
-        }
+      delegate: 'a',
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
     });
-});
+  });
